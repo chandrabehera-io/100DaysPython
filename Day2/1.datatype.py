9502d9b540b1649d281a54ef25991561cdf8933e
Chandra Behera <contact.chandrabehera@gmail.com>
2022-05-15 20:27:17 +0530

    updated

---
+++ Day2/1.datatype.py
@@ -0,0 +1,13 @@
+# string subscripting pullout individual character 
+print("Hello"[-1])
+
+# integer
+print(123_000)
+
+# float decimal number
+pi = 3.14159
+print(pi)
+
+# boolean
+True
+False
---
+++ Day2/2.TypeCasting.py
@@ -0,0 +1,7 @@
+num_char = len(input("Name??"))
+new_num = str(num_char)
+print(new_num+" characters")
+# print(type(num_char))
+
+
+#  str int float
