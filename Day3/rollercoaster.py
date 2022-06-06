229667d563adcab0b6e56e4290dd84735e6fa5ef
Chandra Behera <contact.chandrabehera@gmail.com>
2022-06-06 22:56:11 +0530

    done

---
+++ Day3/rollercoaster.py
@@ -0,0 +1,22 @@
+height = int(input("Enter your Height in cm : "))
+bill=0
+if height>=120 :
+    print("You can ride rollercoaster")
+    age = int(input("What is your age? "))
+    if age<=12:
+        bill= 5
+        photo = input("Do you want a photo? ")
+        print("Ticket price is $5 ")
+    elif age>12 and age<=18:
+        bill= 7
+        print("Ticket price is $7 ")
+    else:
+        bill=15
+        print("Ticket price is $15 ")
+        
+    want_photo=input("Do you want a photo? Y or N : ")
+    if(want_photo=="Y"):
+        bill += 3
+        print(f"Your total bill is ${bill}")
+else :
+    print("You cannot ride rollercoaster")
