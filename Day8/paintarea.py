004cde62bdd75f79fff7b618a42e18df907fc432
Chandra Behera <contact.chandrabehera@gmail.com>
2022-06-20 23:41:24 +0530

    update

--- Day8/paintarea.py
+++ Day8/paintarea.py
@@ -1,7 +1,9 @@
 # Write your code below this line 👇
+import math
+
 
 def paint_calc(height, width, cover):
-    return (height * width)/cover
+    return math.ceil((height * width)/cover)
 
 
 # Write your code above this line 👆
@@ -11,4 +13,4 @@ test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
 res = paint_calc(height=test_h, width=test_w, cover=coverage)
-print(res)
+print(f"You will need {res} liters of paint to paint the wall.")
